15381b94cac31aab026a8ab21cdd0e653d92c8f0
bagaspra <bagasprasetyo36@gmail.com>
2022-10-19 07:59:11 +0800

    env

--- src/client.js
+++ src/client.js
@@ -3,13 +3,13 @@ import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
   // projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  projectId: fjgciggk,
+  projectId: 'fjgciggk',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
   // token: process.env.REACT_APP_SANITY_TOKEN,
   token:
-    sk2cXrwhk8zK0bWzbzQqzX0Z9PhDFH8szKfKypv2w8JWCh7J7zURsCeZtJEiwhXeRqdQ2GFynE4ODT4jTTREcnOfrf2gvhnMhCbx7ORruagSMiAEEBRkymRS8YSQZtxMKXOBTbJIShP0eaqHhYZBKEVhotuNOaATDuZHrPDznYXIuAnO7Q2q,
+    'sk2cXrwhk8zK0bWzbzQqzX0Z9PhDFH8szKfKypv2w8JWCh7J7zURsCeZtJEiwhXeRqdQ2GFynE4ODT4jTTREcnOfrf2gvhnMhCbx7ORruagSMiAEEBRkymRS8YSQZtxMKXOBTbJIShP0eaqHhYZBKEVhotuNOaATDuZHrPDznYXIuAnO7Q2q',
 });
 
 const builder = imageUrlBuilder(client);
